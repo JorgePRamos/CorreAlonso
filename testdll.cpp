ad17e3aac7d8499938e81e32fb60925f64ac4595
Chemi3 <josemmr2009@hotmail.com>
2019-04-28 10:00:06 +0200

    test msg

--- testdll.cpp
+++ testdll.cpp
@@ -9,6 +9,7 @@ gcc object1.o object2.o -lMyLib2 -Wl,-Bstatic -lMyLib1 -Wl,-Bdynamic -o output
 // GetProcAddress to access myPuts from Myputs.dll. 
 
 #include <windows.h> 
+#include <winuser.h>
 #include <stdio.h> 
 /*
 int FALONSO2_inicio(int ret);
@@ -33,14 +34,12 @@ int main( void )
  
     // Get a handle to the DLL module.
  
-    hinstLib = LoadLibrary(TEXT("falonso2.dll")); //cargas la libreria en memoria del proceso
+    //hinstLib = LoadLibrary(TEXT("falonso2.dll")); //cargas la libreria en memoria del proceso
     //HINSTANCE__ *hModule= LoadLibrary(L"C:\\Windows\\System32\\user32.dll");
  
     // If the handle is valid, try to get the function address.
  
-    if (hinstLib != NULL) 
-    { 
-       f_ini = (DLL1Arg) GetProcAddress(hinstLib, "FALONSO2_inicio"); //obtienes y añades la direccion de la funcion
+       /*f_ini = (DLL1Arg) GetProcAddress(hinstLib, "FALONSO2_inicio"); //obtienes y añades la direccion de la funcion
         f_pausa = (DLL0Arg) GetProcAddress(hinstLib, "FALONSO2_pausa");
         // If the function address is valid, call the function.
  
@@ -50,20 +49,37 @@ int main( void )
             fRunTimeLinkSuccess = TRUE;
             //(ProcAdd) (L"2"); 
             printf("entro");
-            f_ini(1);
-        }
-        // Free the DLL module.
- 
-        fFreeResult = FreeLibrary(hinstLib); 
-    } 
+            //f_ini(1);
 
+        }*/
+
+        
+        //if(PostThreadMessageA(GetCurrentThreadId, msg, 0, 0)==FALSE)
+
+
+        if(PostMessageA(HWND_BROADCAST, 0x0402, 0, 0)==FALSE)
+            exit(1);
+        MSG test_msg, uMsg;
+        //if(PeekMessageA(&test_msg, NULL,0x0401, 0x0403, PM_REMOVE )!=0)
+        if(PeekMessageA(&test_msg, NULL, WM_INPUT, 0x0402, PM_REMOVE) !=0)
+            printf("msg recivido\n");
+        // Free the DLL module.
+while (GetMessage (&uMsg, NULL, 0, 0) > 0)
+{
+     TranslateMessage (&uMsg);
+     DispatchMessage (&uMsg);
+}
+
+        //fFreeResult = FreeLibrary(hinstLib); 
+    
+/*
     // If unable to call the DLL function, use an alternative.
     if (! fRunTimeLinkSuccess) 
         printf("Message printed from executable\n"); 
     while (1)
     {
         f_pausa;
-    }
+    }*/
     
     return 0;
 
@@ -73,9 +89,7 @@ int main( void )
 The gcc can link against SUSI4.dll. Use FIND_LIBRARY(DLL_SUSI4 Susi4.dll ${CMAKE_SOURCE_DIR}/lib/susi4) and
  TARGET_LINK_LIBRARIES(proggi ${DLL_SUSI4}) commands in your cmake script. An executable will be created. When you execute your program be shure that the dll is in the same path.*/
 
- using namespace std;
- 
-typedef int(__stdcall MESS)(unsigned int);
+
 
 
 /*
