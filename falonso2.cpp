ab7d9d8cb03ed092eabd7c5ee4db2b1094a80392
Jorge Pérez Ramos <jorgeperezramos99@gmail.com>
2019-05-13 23:20:41 +0200

    V2.0  Arriba España

--- falonso2.cpp
+++ falonso2.cpp
@@ -496,8 +496,8 @@ DWORD WINAPI funcionHilos (LPVOID pEstruct_2){
     for (b = 136; b >= 0;) {//Busqueda de Sitio
         //fprintf(stderr, "Color (%d) [%d] Iteracion b = %d\n", colores[1 + (miIndice - 1) % 6], miIndice, b);
         b -= 2;
-        //if (!(posOcup(miIndiceCarril, b))) {
-        if (arrayPosiciones[b + (miIndiceCarril) * 137]==0) {
+        if (!(posOcup(miIndiceCarril, b))) {
+        //if (arrayPosiciones[b + (miIndiceCarril) * 137]==0) {
 
             //fprintf(stderr, "Color (%d) [%d] Carril libre encontrado\n", colores[1 + (miIndice - 1) % 6], miIndice);
             if (iniCoche( & miIndiceCarril, & b, colores[1 + (miIndice - 1) % 6]) != 0) {
